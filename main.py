7ae6bd1940386862e0cc963f6e2d6cdddfdc1698
hdgiacon <hectordorrighello@gmail.com>
2024-10-03 23:30:49 -0300

    feat(modelo): criação do projeto e primeiro teste com YOLO funcionando
    * detecção funcionou mas classificou incorretamente

---
+++ main.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo11s.pt")
+
+train_results = model.train(
+    data = "dataset/data.yaml",
+    epochs = 50,
+    imgsz = 640,
+    device = "0",
+)
+
+metrics = model.val()
+
+results = model("assets/img_test.jpg")
+results[0].show()
+
+path = model.export(format = "onnx")
